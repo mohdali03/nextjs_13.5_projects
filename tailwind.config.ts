92e45625a2a922bae43d21586ac5b219943bfda2
Shaikh Mohd Ali <91148960+mohdali03@users.noreply.github.com>
2023-11-13 11:09:48 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ module.exports = {
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
+    
     "./src/**/*.{ts,tsx}",
   ],
   theme: {
@@ -73,5 +74,5 @@ module.exports = {
       },
     },
   },
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 };
-
