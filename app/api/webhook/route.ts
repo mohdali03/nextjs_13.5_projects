4bbda70223449be4cefb21e9f6c8d3671052a7b1
Ali <shaikhali6507@gmail.com>
2023-11-14 18:34:59 +0530

    route

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -22,7 +22,7 @@ export async function POST(req: Request) {
 
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response('Error occured -- no svix headers',1{
+    return new Response('Error occured -- no svix headers',{
       status: 400
     })
   }
